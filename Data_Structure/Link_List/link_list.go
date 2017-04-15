7d616308a3983461b62ea2b42273b8c992e9cc7f
Juntaran <jacinthmail@gmail.com>
2017-04-15 09:48:22 +0800

    2017.4.15

--- Data_Structure/Link_List/link_list.go
+++ Data_Structure/Link_List/link_list.go
@@ -300,7 +300,6 @@ func (list *List) PrintList() {
 		fmt.Println()
 	}
 }
-<<<<<<< HEAD
 
 // 对链表中的每个元素执行函数
 func (list *List) DoEach(f func(node ListNode))  {
@@ -320,5 +319,3 @@ func (list *List) Items() []*interface{} {
 	return items
 }
 
-=======
->>>>>>> origin/master
