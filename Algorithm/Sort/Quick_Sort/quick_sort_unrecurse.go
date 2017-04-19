418c4d1e5d534ebcd02d597f61e22f99fa6d40f2
Juntaran <jacinthmail@gmail.com>
2017-04-19 22:40:26 +0800

    2017.4.19
    Update quick_sort_unrecurse

--- Algorithm/Sort/Quick_Sort/quick_sort_unrecurse.go
+++ Algorithm/Sort/Quick_Sort/quick_sort_unrecurse.go
@@ -6,7 +6,7 @@
 
 package Quick_Sort
 
-import "Golang_Algorithm/Data_Structure/Stack"
+//import "Golang_Algorithm/Data_Structure/Stack"
 
 func partition(nums []int, left int, right int) int {
 	// 选第一个元素作为枢纽元
@@ -29,26 +29,88 @@ func partition(nums []int, left int, right int) int {
 }
 
 func Quick_Sort_Unrecurse(nums []int)  {
-	sort_stack := Stack.New()
-	left  := 0
-	right := len(nums) - 1
-	if left < right {
-		sort_stack.Push(left)
-		sort_stack.Push(right)
-		for sort_stack.IsEmpty() == false {
-			j := sort_stack.Top().(int)
-			sort_stack.Pop()
-			i := sort_stack.Top().(int)
-			sort_stack.Pop()
-			k := partition(nums, i, j)
-			if i < k - 1 {
-				sort_stack.Push(i)
-				sort_stack.Push(k-1)
-			}
-			if k + 1 < j {
-				sort_stack.Push(k+1)
-				sort_stack.Push(j)
-			}
+
+/***********************方法1***************************/
+	//sort_stack := Stack.New()
+	//left  := 0
+	//right := len(nums) - 1
+	//if left < right {
+	//	sort_stack.Push(left)
+	//	sort_stack.Push(right)
+	//	for sort_stack.IsEmpty() == false {
+	//		j := sort_stack.Top().(int)
+	//		sort_stack.Pop()
+	//		i := sort_stack.Top().(int)
+	//		sort_stack.Pop()
+	//		k := partition(nums, i, j)
+	//		if i < k - 1 {
+	//			sort_stack.Push(i)
+	//			sort_stack.Push(k-1)
+	//		}
+	//		if k + 1 < j {
+	//			sort_stack.Push(k+1)
+	//			sort_stack.Push(j)
+	//		}
+	//	}
+	//}
+
+/***********************方法2***************************/
+	//// 栈中保存下次需要排序的子数组的开始位置和结束位置
+	//sort_stack := Stack.New()
+	//sort_stack.Push(0)
+	//sort_stack.Push(len(nums) - 1)
+	//
+	//// 栈非空
+	//for sort_stack.IsEmpty() == false {
+	//	high := sort_stack.Top().(int)
+	//	sort_stack.Pop()
+	//	low  := sort_stack.Top().(int)
+	//	sort_stack.Pop()
+	//	middle := partition(nums, low, high)
+	//
+	//	// 右边子数组入栈
+	//	if (middle + 1) < high {
+	//		sort_stack.Push(middle + 1)
+	//		sort_stack.Push(high)
+	//	}
+	//	// 左边数组入栈
+	//	if low < (middle - 1) {
+	//		sort_stack.Push(low)
+	//		sort_stack.Push(middle - 1)
+	//	}
+	//}
+
+/***********************方法3***************************/
+	// 递归层数设置，栈中保存下次需要排序的子数组的开始位置和结束位置
+	var sort_stack = make([]int, len(nums) * 2)
+	top := -1
+	top ++
+	sort_stack[top] = 0
+	top ++
+	sort_stack[top] = len(nums) - 1
+
+	// 栈非空
+	for top > 0 {
+		high := sort_stack[top]
+		top --
+		low  := sort_stack[top]
+		top --
+		middle := partition(nums, low, high)
+
+		// 右边子数组入栈
+		if middle + 1 < high {
+			top ++
+			sort_stack[top] = middle + 1
+			top ++
+			sort_stack[top] = high
+		}
+		// 左边子数组入栈
+		if middle - 1 > low {
+			top ++
+			sort_stack[top] = low
+			top ++
+			sort_stack[top] = middle - 1
 		}
 	}
+
 }
